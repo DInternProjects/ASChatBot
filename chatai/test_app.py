b566b33e2cb27492c951b4b3d3e299d7cac084fb
Aleksandar Stojanovic <acika.sto@gmail.com>
2025-03-11 15:10:36 +0100

    lint comment fix

--- chatai/test_app.py
+++ chatai/test_app.py
@@ -6,8 +6,11 @@ It ensures proper functionality of the app's routes and responses.
 import pytest
 from aschatbotflask import app  # Import your Flask app
 
+
 @pytest.fixture
-def test_client():
+
+def client1():
+
     """
     Provides a test client for the Flask application.
     """
@@ -15,13 +18,13 @@ def test_client():
     with app.test_client() as client:
         yield client
 
-def test_hello_world(test_client):
+def test_hello_world(client2):
     """
     Tests the root ("/") route of the Flask application.
     Ensures the status code is 200 and the response contains the expected text.
     """
     # Make a GET request to the root URL "/"
-    response = test_client.get('/')
+    response = client2.get('/')
     # Assert that the response data matches the expected output
     assert response.status_code == 200
     assert b"Your OpenAI API Key:" in response.data
